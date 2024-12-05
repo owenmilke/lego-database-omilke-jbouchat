25717238b97d5d6038ba5d7648c7510ff570fdb8
heidelbergah <james.bouchat@gmail.com>
2024-12-04 20:22:02 -0500

    Setup complete

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, redirect, url_for, request, render_template, flash
 import requests, json
+import sqlite3
+from flask import Flask, redirect, url_for, request, render_template, flash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '92051f34a4e348cd1311488984333c199abe2e57cc1fa4bf'
+
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
-if __name__ == '__main__':
-    app.run()
+
