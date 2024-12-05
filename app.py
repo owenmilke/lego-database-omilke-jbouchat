afb7a7c7ed00130f59f6194dc18d27300756831f
owenmilke <omilke@uvm.edu>
2024-12-04 20:52:22 -0500

    pandas

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import sqlite3
+import pandas as pd
 from flask import Flask, redirect, url_for, request, render_template, flash, redirect, make_response
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '92051f34a4e348cd1311488984333c199abe2e57cc1fa4bf'
-
 
 @app.route('/')
 def index():
