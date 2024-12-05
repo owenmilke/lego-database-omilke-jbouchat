17a8537662b9953fb043e5f06f74c80cdd15356b
owenmilke <omilke@uvm.edu>
2024-12-04 20:36:39 -0500

    flask imports

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-import requests, json
 import sqlite3
-from flask import Flask, redirect, url_for, request, render_template, flash
+from flask import Flask, redirect, url_for, request, render_template, flash, redirect, make_response
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '92051f34a4e348cd1311488984333c199abe2e57cc1fa4bf'
