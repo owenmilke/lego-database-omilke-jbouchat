351766b8c712dc07804e9d8feaf7de87aedc89e7
heidelbergah <james.bouchat@gmail.com>
2024-12-04 20:51:18 -0500

    Login stuff

--- app.py
+++ app.py
@@ -9,5 +9,16 @@ app.config['SECRET_KEY'] = '92051f34a4e348cd1311488984333c199abe2e57cc1fa4bf'
 def index():
     return render_template('login.html')
 
+
+@app.post('/confirm_credentials')
+def confirm_credentials():
+    return render_template('login.html')
+
+
+@app.post('/create_user')
+def create_user():
+    return render_template('login.html')
+
+
 if __name__ == "__main__":
     app.run()
