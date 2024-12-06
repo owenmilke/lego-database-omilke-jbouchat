bc0889dfd864ad3a1f0ebd5ae9d5f24b9c071aad
owenmilke <omilke@uvm.edu>
2024-12-06 11:41:07 -0500

    most of create user done + more base files

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import sqlite3
-import pandas as pd
 from flask import Flask, redirect, url_for, request, render_template, flash, redirect, make_response
 
 app = Flask(__name__)
@@ -20,6 +19,7 @@ def menu():
     cur = con.cursor()
 
     result = cur.execute("SELECT name FROM users WHERE user_id = ?", (AccountData.user_id,)).fetchone()
+
     cur.close()
     con.close()
 
@@ -28,12 +28,20 @@ def menu():
 
 @app.route('/view')
 def view():
+    # for viewing existing listings
     return render_template('view.html')
 
 
 @app.route('/add')
 def add():
+    # for adding a new listing
     return render_template('add.html')
+
+
+@app.route('/edit')
+def edit():
+    # for adding email or changing username/password
+    return render_template('edit.html')
 
 
 @app.post('/confirm_credentials')
@@ -52,6 +60,7 @@ def confirm_credentials():
     cur = con.cursor()
 
     result = cur.execute("SELECT user_id FROM users WHERE name = ? AND password = ?", (username, password)).fetchone()
+
     cur.close()
     con.close()
 
@@ -65,7 +74,37 @@ def confirm_credentials():
 
 @app.post('/create_user')
 def create_user():
-    return render_template('login.html')
+    username = request.form["c_username"]
+    password = request.form["c_password"]
+    
+    if (username == ""):
+        flash("Username cannot be empty.")
+        return redirect("/")
+    if (password == ""):
+        flash("Password cannot be empty.")
+        return redirect("/")
+    
+    con = sqlite3.connect("XBayDB")
+    cur = con.cursor()
+
+    result = cur.execute("SELECT name FROM users WHERE name = ?", (username,)).fetchone()
+    if (result is not None):
+        flash("Username already exists. Please try another.")
+        return redirect("/")
+    
+    # cur.execute("INSERT INTO users(name, password) VALUES(?, ?)", (username, password))
+
+    result = cur.execute("SELECT user_id FROM users WHERE name = ? AND password = ?", (username, password)).fetchone()
+
+    cur.close()
+    con.close()
+
+    if result != None:
+        AccountData.user_id = str(result[0])
+        return menu()
+    else:
+        flash("Login invalid.")
+        return redirect("/")
 
 
 if __name__ == "__main__":
